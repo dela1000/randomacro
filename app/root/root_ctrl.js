7290cd13a12d432c10531411dea7d98bffce7831
dela1000 <dela1000@gmail.com>
2017-02-12 14:14:48 -0800

    Update random list logic

--- app/root/root_ctrl.js
+++ app/root/root_ctrl.js
@@ -1,30 +1,33 @@
 var app = angular.module('app');
 
-app.controller('rootController', function($scope, rootServices) {
+app.controller('rootController', function($scope, rootServices, alert) {
     $scope.formData = {};
     $scope.formData.level = "beginner"
 
     $scope.$watch('formData', function() {
+        if($scope.formData.numberInput < 2){
+            alert.addAlert("You want to move, right? Pick more than 1")
+            return;
+        }
+        if($scope.formData.numberInput > 10){
+            alert.addAlert("take it easy, champ. Pick less than 10")
+            return;
+        }
         if ($scope.formData.numberInput) {
             var randomNumberList = [];
             var filteredList = [];
             $scope.randomList = [];
-            if($scope.formData.level !== "all"){
-                filteredList = _.filter(rootServices.positionsList, function(item) { 
-                    return item.difficulty === $scope.formData.level; 
+            if ($scope.formData.level !== "all") {
+                filteredList = _.filter(rootServices.positionsList, function(item) {
+                    return item.difficulty === $scope.formData.level;
                 });
             } else {
                 filteredList = rootServices.positionsList;
             }
-            
-            for (i = 0; i < $scope.formData.numberInput; i++) {
-                var randomNumbergenerated = _.random(0, filteredList.length-1);
-                randomNumberList.push(randomNumbergenerated)
-            }
-            _.forEach(randomNumberList, function(number) {
-                var temp = filteredList[number];
-                $scope.randomList.push(temp)
-            })
+
+            _.times($scope.formData.numberInput, function(){
+                $scope.randomList.push(filteredList[_.random(0, filteredList.length - 1)])
+            });
         }
     }, true)
 });
