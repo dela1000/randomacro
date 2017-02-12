32554ec11b2a04d64d65711882f0b37d5bd19f60
dela1000 <dela1000@gmail.com>
2017-02-12 14:03:46 -0800

    Add data, and create random list logic

--- app/root/root_ctrl.js
+++ app/root/root_ctrl.js
@@ -1,7 +1,30 @@
 var app = angular.module('app');
 
 app.controller('rootController', function($scope, rootServices) {
+    $scope.formData = {};
+    $scope.formData.level = "beginner"
 
-    
+    $scope.$watch('formData', function() {
+        if ($scope.formData.numberInput) {
+            var randomNumberList = [];
+            var filteredList = [];
+            $scope.randomList = [];
+            if($scope.formData.level !== "all"){
+                filteredList = _.filter(rootServices.positionsList, function(item) { 
+                    return item.difficulty === $scope.formData.level; 
+                });
+            } else {
+                filteredList = rootServices.positionsList;
+            }
+            
+            for (i = 0; i < $scope.formData.numberInput; i++) {
+                var randomNumbergenerated = _.random(0, filteredList.length-1);
+                randomNumberList.push(randomNumbergenerated)
+            }
+            _.forEach(randomNumberList, function(number) {
+                var temp = filteredList[number];
+                $scope.randomList.push(temp)
+            })
+        }
+    }, true)
 });
-
--- app/root/root_services.js
+++ app/root/root_services.js
@@ -3,6 +3,170 @@ var app = angular.module('app');
 app.factory('rootServices', function($http, $rootScope) {
     var services = {};
 
-    
+    services.positionsList = [{
+        "name": "Bird",
+        "difficulty": "beginner",
+        "on": "hands and feet",
+        "inverted": false,
+        "rating": 1.0
+    }, {
+        "name": "Throne",
+        "difficulty": "beginner",
+        "on": "hands and feet",
+        "inverted": false,
+        "rating": 1.0
+    }, {
+        "name": "Straddle Throne",
+        "difficulty": "beginner",
+        "on": "feet",
+        "inverted": false,
+        "rating": 2.0
+    }, {
+        "name": "Vishnu\'s Couch",
+        "difficulty": "beginner",
+        "on": "feet",
+        "inverted": false,
+        "rating": 1.0
+    }, {
+        "name": "High-Flying Whale",
+        "difficulty": "beginner",
+        "on": "hands and feet",
+        "inverted": false,
+        "rating": 1.0
+    }, {
+        "name": "Side Star",
+        "difficulty": "intermediate",
+        "on": "hands and feet",
+        "inverted": false,
+        "rating": 2.0
+    }, {
+        "name": "Star",
+        "difficulty": "intermediate",
+        "on": "hands and feet",
+        "inverted": true,
+        "rating": 2.0
+    }, {
+        "name": "Shoulder Stand",
+        "difficulty": "advanced",
+        "on": "hands",
+        "inverted": true,
+        "rating": 4.0
+    }, {
+        "name": "Supported Shoulder Stand",
+        "difficulty": "intermediate",
+        "on": "hands and feet",
+        "inverted": true,
+        "rating": 2.0
+    }, {
+        "name": "Reverse Shoulder Stand",
+        "difficulty": "advanced",
+        "on": "hands and feet",
+        "inverted": true,
+        "rating": 4.0
+    }, {
+        "name": "Cathedral",
+        "difficulty": "beginner",
+        "on": "hands and feet",
+        "inverted": true,
+        "rating": 1.0
+    }, {
+        "name": "Back Bird",
+        "difficulty": "beginner",
+        "on": "feet",
+        "inverted": false,
+        "rating": 1.0
+    }, {
+        "name": "Straddle Bat",
+        "difficulty": "beginner",
+        "on": "feet",
+        "inverted": true,
+        "rating": 1.0
+    }, {
+        "name": "Floating Poshi",
+        "difficulty": "intermediate",
+        "on": "hands",
+        "inverted": true,
+        "rating": 3.0
+    }, {
+        "name": "Folded Leaf",
+        "difficulty": "beginner",
+        "on": "feet",
+        "inverted": true,
+        "rating": 1.0
+    }, {
+        "name": "Foot-to-Hand",
+        "difficulty": "intermediate",
+        "on": "hands",
+        "inverted": false,
+        "rating": 4.0
+    }, {
+        "name": "Extended Foot-to-Hand",
+        "difficulty": "advanced",
+        "on": "hands",
+        "inverted": false,
+        "rating": 3.0
+    }, {
+        "name": "Shin-to-Foot",
+        "difficulty": "beginner",
+        "on": "feet",
+        "inverted": false,
+        "rating": 1.0
+    }, {
+        "name": "Foot-to-Shin",
+        "difficulty": "beginner",
+        "on": "feet",
+        "inverted": false,
+        "rating": 1.0
+    }, {
+        "name": "Foot-to-Foot",
+        "difficulty": "intermediate",
+        "on": "feet",
+        "inverted": false,
+        "rating": 3.0
+    }, {
+        "name": "Hand-to-Hand",
+        "difficulty": "advanced",
+        "on": "hands",
+        "inverted": true,
+        "rating": 4.0
+    }, {
+        "name": "Bat",
+        "difficulty": "beginner",
+        "on": "feet",
+        "inverted": true,
+        "rating": 1.0
+    }, {
+        "name": "Couch",
+        "difficulty": "intermediate",
+        "on": "feet",
+        "inverted": false,
+        "rating": 2.0
+    }, {
+        "name": "Reverse Star",
+        "difficulty": "advanced",
+        "on": "feet",
+        "inverted": true,
+        "rating": 3.0
+    }, {
+        "name": "Tittibhasana",
+        "difficulty": "advanced",
+        "on": "hands",
+        "inverted": false,
+        "rating": 4.0
+    }, {
+        "name": "Floating Camel",
+        "difficulty": "advanced",
+        "on": "hands",
+        "inverted": false,
+        "rating": 4.0
+    }, {
+        "name": "Croc",
+        "difficulty": "advanced",
+        "on": "hands",
+        "inverted": false,
+        "rating": 5.0
+    }];
+
+
     return services;
 })
