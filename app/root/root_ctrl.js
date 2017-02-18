e011510b45958fc8b3f34cb533ddf395a5378867
dela1000 <dela1000@gmail.com>
2017-02-18 01:04:52 -0800

    Update buttons

--- app/root/root_ctrl.js
+++ app/root/root_ctrl.js
@@ -11,13 +11,14 @@ app.controller('rootController', function($scope, positionsServices, alert) {
     })
 
     $scope.$watch('formData.all', function() {
+        console.log("+++ 14 root_ctrl.js Here")
         if ($scope.formData.all) {
             $scope.formData.diff.beginner = false;
             $scope.formData.diff.intermediate = false;
             $scope.formData.diff.advanced = false;
         }
 
-        selectPositions();
+        runSelectPositions();
     }, true)
 
     $scope.$watchGroup(['formData.diff.beginner', 'formData.diff.intermediate', 'formData.diff.advanced'], function() {
@@ -29,8 +30,22 @@ app.controller('rootController', function($scope, positionsServices, alert) {
             $scope.formData.diff = {};
         }
 
-        selectPositions();
+        if (!$scope.formData.diff.beginner && !$scope.formData.diff.intermediate && !$scope.formData.diff.advanced) {
+            $scope.formData.all = true;
+        }
+
+        runSelectPositions();
     }, true)
+
+
+    function runSelectPositions() {
+        if ($scope.formData.diff.beginner || $scope.formData.diff.intermediate || $scope.formData.diff.advanced || $scope.formData.all) {
+            selectPositions();
+        } else {
+            $scope.filteredList = [];
+            initVars();
+        };
+    };
 
     var selectPositions = function() {
         //If there is no numberInput data, clear variables and return
@@ -49,50 +64,52 @@ app.controller('rootController', function($scope, positionsServices, alert) {
         }
         //if numberInput is valid and level is selected display data
         if ($scope.formData.numberInput) {
-            var filteredList = [];
+
+            $scope.filteredList = [];
             //clear current data
             initVars();
+            console.log("+++ 67 root_ctrl.js $scope.positionHolder.firstRowList.length: ", $scope.positionHolder.firstRowList.length)
             //if all is selected, use all the positions on the list
             if ($scope.formData.all) {
-                filteredList = positionsServices.positionsList;
+                $scope.filteredList = positionsServices.positionsList;
             }
             //if any of the difficulty levels is selected, filter the position list by the difficulty's selected
             if ($scope.formData.diff.beginner === true || $scope.formData.diff.intermediate === true || $scope.formData.diff.advanced === true) {
                 var holder = [];
                 _.forEach($scope.formData.diff, function(level, index) {
-                    if(level === true){
+                    if (level === true) {
                         holder.push(_.filter(positionsServices.positionsList, function(item) {
                             return item.difficulty === index;
                         }))
                     }
                 })
-                filteredList = _.flattenDeep(holder)
+                $scope.filteredList = _.flattenDeep(holder)
             }
             //Number of possible positions
-            $scope.possiblePositions = filteredList.length;
+            $scope.possiblePositions = $scope.filteredList.length;
             //randomize the final list
-            var tempRandomizedList = _.shuffle(filteredList);
+            var tempRandomizedList = _.shuffle($scope.filteredList);
             //Depending on what numberInput the user selected, add up to 4 items in each positionHolder, and push rating to ratings scope
             _.times($scope.formData.numberInput, function(index) {
                 if (index <= 3) {
                     $scope.positionHolder.firstRowList.push(tempRandomizedList[index])
-                        $scope.ratings.push(tempRandomizedList[index].rating)
+                    $scope.ratings.push(tempRandomizedList[index].rating)
                 } else if (index > 3 && index <= 7) {
                     $scope.positionHolder.secondRowList.push(tempRandomizedList[index])
-                        $scope.ratings.push(tempRandomizedList[index].rating)
+                    $scope.ratings.push(tempRandomizedList[index].rating)
                 } else {
                     $scope.positionHolder.thirdRowList.push(tempRandomizedList[index])
-                        $scope.ratings.push(tempRandomizedList[index].rating)
+                    $scope.ratings.push(tempRandomizedList[index].rating)
                 };
             });
             //per each positionHolder, set the appropriate class to display the pills correctly width-wise
             _.forEach($scope.positionHolder, function(list) {
-                assignPillClass(list, list.length);
-            })
-            //find the mean of all the selected the ratings
+                    assignPillClass(list, list.length);
+                })
+                //find the mean of all the selected the ratings
             $scope.difficultyRating = _.mean($scope.ratings);
         }
-    }
+    };
     // HELPER FUNCTIONS
     //Clear all the list and rating variables
     var initVars = function() {
@@ -105,7 +122,7 @@ app.controller('rootController', function($scope, positionsServices, alert) {
         $scope.ratings = [];
         $scope.difficultyRating = null;
         $scope.possiblePositions = null;
-    }
+    };
     //assign the correct class depending on how many items are in each positionHolder
     var assignPillClass = function(collection, length) {
         if (length === 1) {
