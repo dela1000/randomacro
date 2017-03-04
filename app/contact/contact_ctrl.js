f65030eb56f3dbb605112f099dec1349dc085553
dela1000 <dela1000@gmail.com>
2017-03-04 12:16:38 -0500

    Send email from contact form

--- app/contact/contact_ctrl.js
+++ app/contact/contact_ctrl.js
@@ -1,14 +1,19 @@
 var app = angular.module('app');
 
-app.controller('contactController', function($scope, alert) {
-    
-
-    $scope.test = function () {
-      console.log("+++ 7 contact_ctrl.js $scope.form: ", $scope.form)
-    }
+app.controller('contactController', function($scope, alert, $http) {
 
 
     $scope.sendMail = function() {
-        
+        var data = {
+            name: $scope.name,
+            email: $scope.email,
+            position: $scope.position,
+            difficulty: $scope.difficulty,
+            link: $scope.link
+        }
+        $http.post('http://localhost:8080/contactForm', data)
+            .then(function(result) {
+                console.log("result: ", JSON.stringify(result, null, "\t"));
+            })
     }
 })
--- backend/routes.js
+++ backend/routes.js
@@ -1,11 +1,45 @@
 var router = require('express').Router();
 var path = require('path');
+var secrets = require('./secrets.js');
+var nodemailer = require('nodemailer');
+var transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+        user: secrets.gmailAddress,
+        pass: secrets.gmailPass
+    }
+});
 
 
 //Routes
 //Home route
 router.get('/', function(request, response) {
     response.status(202).sendFile(path.resolve("app/index.html"));
+});
+
+router.post('/contactForm', function(req, res) {
+    console.log("+++ 14 routes.js req.body: ", req.body)
+
+
+    var mailOptions = {
+        from: req.body.email,
+        to: secrets.gmailAddress,
+        subject: 'Message from ' + req.body.name,
+        text: req.body.name + " " + req.body.email + " " + req.body.position + " " + req.body.difficulty + " " + req.body.link
+    }
+
+    transporter.sendMail(mailOptions, function(error, response) {
+        if (error) {
+            console.log("error");
+            console.log(error);
+            res.send(error);
+        } else {
+            console.log("Message sent: " + response.message);
+            res.send(response);
+        }
+    });
+
+
 });
 
 module.exports = router;
