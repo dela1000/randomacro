d6900d48e43f8d281de5e8aec03061a32873c7a3
daniel <dela1000@gmail.com>
2019-02-12 23:50:23 +0100

    revert to site design

--- app/root/root_ctrl.js
+++ app/root/root_ctrl.js
@@ -4,24 +4,11 @@ app.controller('rootController', function($scope, $window, $stateParams, positio
     $scope.formData = {};
 
     $scope.showNav = true;
-    $scope.hideNavigation = () => {
+    $scope.hideNavigation = function() {
         $scope.showNav = !$scope.showNav;
     }
 
-    $scope.numbers = _.range(2, 11);
-
-    _.forEach($scope.numbers, (number, index) => {
-        $scope.numbers[index] = {
-            number: number
-        }
-    })
-
-    $scope.numberSelected = (number) => {
-        console.log("+++ 20 root_ctrl.js number: ", number)
-        $scope.formData.numberInput = number
-    }
-
-    $scope.levelAll = () => {
+    $scope.levelAll = function() {
         $window.scrollTo(0, 0);
         $scope.formData.all = true;
         $scope.formData.diff.beginner = false;
@@ -29,7 +16,8 @@ app.controller('rootController', function($scope, $window, $stateParams, positio
         $scope.formData.diff.advanced = false;
     }
 
-    $scope.levelSelect = (level) => {
+
+    $scope.levelSelect = function(level) {
         $window.scrollTo(0, 0);
         $scope.formData.all = false;
 
@@ -40,7 +28,7 @@ app.controller('rootController', function($scope, $window, $stateParams, positio
         }
     }
 
-    $scope.$watch('formData.all', () => {
+    $scope.$watch('formData.all', function() {
         if ($scope.formData.all) {
             $scope.formData.diff.beginner = false;
             $scope.formData.diff.intermediate = false;
@@ -48,7 +36,7 @@ app.controller('rootController', function($scope, $window, $stateParams, positio
         }
     }, true)
 
-    $scope.$watchGroup(['formData.diff.beginner', 'formData.diff.intermediate', 'formData.diff.advanced'], () => {
+    $scope.$watchGroup(['formData.diff.beginner', 'formData.diff.intermediate', 'formData.diff.advanced'], function() {
         if ($scope.formData.diff.beginner || $scope.formData.diff.intermediate || $scope.formData.diff.advanced) {
             $scope.formData.all = false;
         }
@@ -62,16 +50,16 @@ app.controller('rootController', function($scope, $window, $stateParams, positio
         }
     }, true)
 
-    $scope.saveList = () => {
+    $scope.saveList = function() {
         var arrays = [];
-        _.forEach($scope.positionHolder, (array) => {
+        _.forEach($scope.positionHolder, function(array) {
             arrays.push(array)
         })
         var flattenArray = _.flattenDeep(arrays);
 
         var indexesSelected = "";
-        _.forEach(flattenArray, (selectedPosition) => {
-            _.forEach(positionsServices.positionsList, (positionInList, index) => {
+        _.forEach(flattenArray, function(selectedPosition) {
+            _.forEach(positionsServices.positionsList, function(positionInList, index) {
                 if (selectedPosition.name === positionInList.name) {
                     indexesSelected += index + ","
                 }
@@ -84,18 +72,18 @@ app.controller('rootController', function($scope, $window, $stateParams, positio
         alert.addAlert('A link to this flow has been added to your clipboard. You may save or share it.', "calm")
     }
 
-    $scope.lockList = () => {
+    $scope.lockList = function() {
         $scope.locked = !$scope.locked;
     }
 
-    $scope.refresh = () => {
+    $scope.refresh = function() {
         if ($scope.formData.numberInput === 1) {
             $scope.formData.numberInput = 4;
         }
         runSelectPositions();
     }
 
-     runSelectPositions = () => {
+    function runSelectPositions() {
         if ($scope.locked) {
             return;
         }
@@ -108,7 +96,7 @@ app.controller('rootController', function($scope, $window, $stateParams, positio
         };
     };
 
-    var selectPositions = () => {
+    var selectPositions = function() {
         if ($scope.locked) {
             return;
         }
@@ -142,9 +130,9 @@ app.controller('rootController', function($scope, $window, $stateParams, positio
             //if any of the difficulty levels is selected, filter the position list by the difficulty's selected
             if ($scope.formData.diff.beginner === true || $scope.formData.diff.intermediate === true || $scope.formData.diff.advanced === true) {
                 var holder = [];
-                _.forEach($scope.formData.diff, (level, index) => {
+                _.forEach($scope.formData.diff, function(level, index) {
                     if (level === true) {
-                        holder.push(_.filter(positionsServices.positionsList, (item) => {
+                        holder.push(_.filter(positionsServices.positionsList, function(item) {
                             return item.difficulty === index;
                         }))
                     }
@@ -160,14 +148,14 @@ app.controller('rootController', function($scope, $window, $stateParams, positio
         }
     };
 
-    $scope.loadBuckets = (list, length) => {
+    $scope.loadBuckets = function(list, length) {
         var times;
         if (length) {
             times = length;
         } else {
             times = $scope.formData.numberInput;
         };
-        _.times(times, (index) => {
+        _.times(times, function(index) {
             if (index <= 3) {
                 $scope.positionHolder.firstRowList.push(list[index])
                 $scope.ratings.push(list[index].rating)
@@ -180,16 +168,16 @@ app.controller('rootController', function($scope, $window, $stateParams, positio
             };
         });
         //per each positionHolder, set the appropriate class to display the pills correctly width-wise
-        _.forEach($scope.positionHolder, (list) => {
+        _.forEach($scope.positionHolder, function(list) {
                 assignPillClass(list, list.length);
             })
             //find the mean of all the selected the ratings
         $scope.difficultyRating = _.mean($scope.ratings);
     }
 
-    // HELPER => S
+    // HELPER FUNCTIONS
     //Clear all the list and rating variables
-    var initVars = () => {
+    var initVars = function() {
         $scope.positionHolder = {
             firstRowList: [],
             secondRowList: [],
@@ -201,24 +189,24 @@ app.controller('rootController', function($scope, $window, $stateParams, positio
         $scope.possiblePositions = null;
     };
     //assign the correct class depending on how many items are in each positionHolder
-    var assignPillClass = (collection, length) => {
+    var assignPillClass = function(collection, length) {
         if (length === 1) {
-            _.forEach(collection, (item) => {
+            _.forEach(collection, function(item) {
                 item.class = "col-md-offset-4 col-md-4"
             })
         }
         if (length === 2) {
-            _.forEach(collection, (item) => {
+            _.forEach(collection, function(item) {
                 item.class = "col-md-offset-2 col-md-3";
             })
         }
         if (length === 3) {
-            _.forEach(collection, (item) => {
+            _.forEach(collection, function(item) {
                 item.class = "col-md-4";
             })
         }
         if (length >= 4) {
-            _.forEach(collection, (item) => {
+            _.forEach(collection, function(item) {
                 item.class = "col-md-3";
             })
         }
@@ -231,10 +219,10 @@ app.controller('rootController', function($scope, $window, $stateParams, positio
         var loadedList = [];
         var test = $stateParams.list.split(",");
 
-        _.forEach(test, (listIndex) => {
+        _.forEach(test, function(listIndex) {
             var digit = Number(listIndex);
             if (digit !== NaN) {
-                _.forEach(positionsServices.positionsList, (position, index) => {
+                _.forEach(positionsServices.positionsList, function(position, index) {
                     if (index === digit) {
                         loadedList.push(position)
                     }
